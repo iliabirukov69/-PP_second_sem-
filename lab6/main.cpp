8acecdad00f83ec34823ee1a9ee12b5366a24e43
iliabirukov69 <ilabirukov69@gmail.com>
2026-09-05 17:57:48 +0200

    Create main.cpp

---
+++ lab6/main.cpp
@@ -0,0 +1,12 @@
+#include "functions.h"
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication::setAttribute(Qt::AA_Use96Dpi);
+
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    return a.exec();
+}
