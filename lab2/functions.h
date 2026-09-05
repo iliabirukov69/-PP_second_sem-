81e8aa2c7e78328aa478bb8001109a952bd6a4d0
iliabirukov69 <ilabirukov69@gmail.com>
2026-09-05 12:08:48 +0200

    Create functions.h

---
+++ lab2/functions.h
@@ -0,0 +1,37 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+#include <stdexcept>
+
+namespace mt {
+class Matrix {
+public:
+    Matrix();
+    Matrix(int rows, int cols, int value = 0);
+    Matrix(const Matrix& other);
+    Matrix& operator=(const Matrix& other);
+    ~Matrix();
+
+    int& at(int i, int j);
+
+    int getRows() const;
+    int getCols() const;
+    bool isEmpty() const;
+
+    void fill(int value);
+
+    // Вместо print() в консоль, метод теперь собирает матрицу в строку для окошка
+    std::string toString() const;
+
+private:
+    void clear();
+    void copyFrom(const Matrix& other);
+
+private:
+    int rows_;
+    int cols_;
+    int** M_;
+};
+}
+
+#endif // FUNCTIONS_H
